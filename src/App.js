fd7d26f03d8a679b2416a7969ce8c0fd96fdcce1
Jiří Strnadel <Juzba88@gmail.com>
2025-02-10 13:25:53 +0100

    React 2 input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [userName, setUserName] = useState("");
+
+	const formSubmibmit = (e) => {
+		e.preventDefault();
+		console.log(e);
+	};
+
+	return (
+		<article className='App'>
+			<form>
+				<input type='text' className='userName' placeholder='Jméno' />
+				<input onClick={formSubmibmit} type='submit' value='Odeslat' />
+			</form>
+		</article>
+	);
+};
 
 export default App;
